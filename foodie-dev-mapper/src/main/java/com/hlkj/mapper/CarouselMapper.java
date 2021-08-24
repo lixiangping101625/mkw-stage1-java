509b3a0810182f57c407008bd648ee209b1477b7
lixiangping <admin>
2021-08-24 21:37:41 +0800

    Carousel逆向生成

---
+++ foodie-dev-mapper/src/main/java/com/hlkj/mapper/CarouselMapper.java
@@ -0,0 +1,9 @@
+package com.hlkj.mapper;
+
+import com.hlkj.my.mapper.MyMapper;
+import com.hlkj.pojo.Carousel;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CarouselMapper extends MyMapper<Carousel> {
+}
